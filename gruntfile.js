77a8fcc5f656aa04685516bb70c124e7e3cb833e
Mateusz <mateusz.gibki@gmail.com>
2018-04-09 21:10:09 +0200

    09.03 new project files

---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass'
+        }
+      }
+    },
+    autoprefixer: {
+      dist: {
+        files: {
+          'css/style.css': 'css/style.css'
+        }
+      }
+    },
+    imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'img/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
+    watch: {
+      css: {
+        files: '**/*.sass',
+        tasks: ['sass']
+      }
+    }
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // Default task(s).
+  grunt.registerTask('default', ["sass", "autoprefixer","imagemin", "watch"]);
+};
