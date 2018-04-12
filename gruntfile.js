8fa626ac137c7725708a81067b8aa897b490c15c
Mateusz <mateusz.gibki@gmail.com>
2018-04-12 22:37:41 +0200

    12.03 changes

--- gruntfile.js
+++ gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
   				expand: true,
   				cwd: 'img/',
   				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
+  				dest: 'images/'
   			}]
   		}
   	},
