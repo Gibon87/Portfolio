8979112774640e6a36c18cb1206107d66eea553a
Mateusz <mateusz.gibki@gmail.com>
2018-04-13 23:12:36 +0200

    13.03 changes

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 140) {
-      $("header").css("background-color", "#e85151");
+      $("header").css("background-color", "#e85151") && $("header").css("height", "12vh");
     } else {
-      $("header").css("background-color", "transparent");
+      $("header").css("background-color", "transparent") && $("header").css("height", "20vh");
     }
   })
 })
