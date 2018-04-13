a74701452a89f6bd79768cdd7d847a2b8587367a
Mateusz <mateusz.gibki@gmail.com>
2018-04-13 22:26:58 +0200

    13.03 changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-
 // CHANGING HEADER BACKGROUND COLOR FUNCTION
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 140) {
-	    $("header").css("background-color" , "#e85151");
-	  }
-	  else{
-		  $("header").css("background-color" , "transparent");
-	  }
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 140) {
+      $("header").css("background-color", "#e85151");
+    } else {
+      $("header").css("background-color", "transparent");
+    }
   })
 })
