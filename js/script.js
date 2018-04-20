723001ff272315bada1e897d49fb1f57c23e1844
Mateusz <mateusz.gibki@gmail.com>
2018-04-20 08:29:47 +0200

    20.04 changes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 10) {
-      $("header").css("background-color", "#ffd600") && $("header").css("height", "12vh");
+      $("header").css("background-color", "#272727") && $("header").css("height", "12vh");
     } else {
       $("header").css("background-color", "transparent") && $("header").css("height", "20vh");
     }
   })
 })
 
-// NOW I'M THINKING THAT THAT SCRITP IS CRAP :) I'M GONNA CHANGE THAT TO, ADD CLASS SCRIPT... HMM 
+// NOW I'M THINKING THAT THAT SCRITP IS CRAP :) I'M GONNA CHANGE THAT TO, ADD CLASS SCRIPT... HMM
